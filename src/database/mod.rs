74b37fee3b71e0f3a6bf66f70ffb8473984735c6
Julien Bui <161204940+Julien-Bui@users.noreply.github.com>
2026-07-10 09:40:09 +0200

    mettre en place setup et les fonctions signatures

---
+++ src/database/mod.rs
@@ -0,0 +1,27 @@
+use rusqlite::Connection;
+
+pub fn init_db() -> rusqlite::Result<()>
+{
+    
+    Ok(())
+}
+
+pub fn add_skill()
+{
+    
+}
+
+pub fn get_all_skills()
+{
+    
+}
+
+pub fn delete_skill()
+{
+    
+}
+
+pub fn save_api_key()
+{
+    
+}
